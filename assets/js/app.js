db37901e89be6fa06ea96420166693e2ee35e20a
具志堅雅子 <amour.vous.0125@gmail.com>
2020-05-20 15:07:30 +0900

    完成

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,49 +11,32 @@
 //   // 通信が失敗した場合のブロック
 //   // 何かしら変数名・・・失敗の原因となるエラー文
 // })
-
-
 $(function () {
     // 検索ボタンがクリックされたら
     $('#search-btn').click(() => {
-        
-        // 入力された郵便番号を取得
-        let val = $('#searhe-word').val();
-        
-
-
-        // Ajsxを開始(郵便番号APIから、住所の除法を取得する)
-
-        $.ajax({
-            url: 'https://zipcloud.ibsnet.co.jp/api/search',
-            type: 'GET',  //通信方法（GTE or POST）
-            dataType: 'jsonp',  //通信結果のデータ形式（例：html, json, jsonp, xml）
-            data: {    //通信先に送信するパラメーター
-                 //パラメーター名：値
-                zipcode: val
-            }   
-
-        }).done((data) => {
-            console.log(data)
-            console.log(data.status)
-            console.log(data.results[0].address1)
-            console.log(data.results[0].address2)
-            console.log(data.results[0].address3)
-            
-            $('#prefecture').text(data.results[0].address1);
-            $('#city').text(data.results[0].address2);
-            $('#add').text(data.results[0].address3);
-
-
-        }).fail((error) => {
-            console.error(error)
-        })
-
-
-        
-    //    $('#prefecture').prepend(val);
-    //    $('#city').prepend(val);
-    //    $('#add').prepend(val)
+      // Loadingを表示する
+      $.LoadingOverlay("show");
+      // 入力された郵便番号を取得
+      let val = $('#search-word').val()
+      // Ajaxを開始（郵便番号APIから、住所の情報を取得する）
+      $.ajax({
+        url: 'https://zipcloud.ibsnet.co.jp/api/search', // 通信先のURL（日本郵便APIのURL）
+        type: 'GET', // 通信方法（GET or POST）
+        dataType: 'jsonp', // 通信結果のデータ形式(例：html, json, jsonp, xml)
+        data: { // 通信先に送信するパラメータ
+          // パラメータ名：値
+          zipcode: val
+        }
+      }).done((data) => {
+        $("#prefecture").text(data.results[0].address1);
+        $("#city").text(data.results[0].address2);
+        $("#address").text(data.results[0].address3);
+      }).fail((error) => {
+        console.error(error)
+      }).always(() => {
+        // 通信が成功した場合、失敗した場合、両方の最後に実行
+        // Loadingを非表示にする
+        $.LoadingOverlay("hide");
+      })
     })
-
-})
+  })
